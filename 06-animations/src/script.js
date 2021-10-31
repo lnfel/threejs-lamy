e67753fdcc5d47794bc2c87446fc6d4c73556df9
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2021-10-31 14:46:04 +0800

    Animate vector3 properties relative to elapsed time

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -29,4 +29,46 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+// Time
+//let time = Date.now()
+
+// CLock
+const clock = new THREE.Clock()
+
+// Animation
+const tick = () =>
+{
+    // Using time to update animation regardless of hardware's frames per second capability
+    //const currentTime = Date.now()
+    //const deltaTime = currentTime - time
+    //time = currentTime
+
+    // CLock
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    //mesh.rotation.y += 0.01
+    //mesh.rotation.y += 0.001 * deltaTime
+
+    // one revolution per second
+    //mesh.rotation.y = elapsedTime * Math.PI * 2
+    //mesh.rotation.y = elapsedTime
+
+    // Playing with sin(x) and cos(x)
+    //mesh.position.y = Math.sin(elapsedTime)
+    //mesh.position.x = Math.cos(elapsedTime)
+
+    // Interesting
+    camera.position.y = Math.sin(elapsedTime)
+    camera.position.x = Math.cos(elapsedTime)
+    camera.lookAt(mesh.position)
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
