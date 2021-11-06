3506ff23b01683af9f9568cba78bc8e6c70a9f7b
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2021-11-06 13:46:22 +0800

    Using cameras and orbit controls, part 07 done

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -1,5 +1,24 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+/**
+ * Cursor
+ * Detect mouse position by listening to mousemove event
+ */
+ const cursor = {
+    x: 0,
+    y: 0,
+ }
+window.addEventListener('mousemove', (event) => {
+    // set cursor to have position from -0.5 to 0.5 on either x or y axis
+    // this way we can set camera position relative to threejs position's
+    // unit of measurement
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = - (event.clientY / sizes.height - 0.5)
+    //console.log('clientX', event.clientX, 'clientY', event.clientY)
+    //console.log(cursor)
+})
 
 /**
  * Base
@@ -24,12 +43,32 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 100)
+
+// Sample orthograpic camera
+/*const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.OrthographicCamera(
+    -1 * aspectRatio,
+    1 * aspectRatio,
+    1,
+    -1,
+    1,
+    100
+)*/
+
+//camera.position.x = 2
+//camera.position.y = 2
 camera.position.z = 2
 camera.lookAt(mesh.position)
 scene.add(camera)
+
+//console.log(camera.position.length())
+
+// OrbitControls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+//controls.target.y = 1
+//controls.update()
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -45,7 +84,29 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    //mesh.rotation.y = elapsedTime;
+
+    // Update camera
+    // cursor position multiplied by desired amount of amplitude
+    // so we can see most of the sides of the cube except for its back
+    //camera.position.x = cursor.x * 10
+    //camera.position.y = cursor.y * 10
+    //camera.lookAt(mesh.position)
+
+    // make the camera rotate around the cube object by updating
+    // camera's x and z axis using sin() and cos() given the angle
+    // of cursor.x multiplied by pi times two then finally multiplied
+    // again by 3 which is our desired amplitude
+    // this results in our cube rotating only one revolution if we move our
+    // mouse from left to right
+    /*camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    camera.position.y = cursor.y * 5
+    camera.lookAt(mesh.position)*/
+
+    // Update controls
+    // reason why update is in tick function is that damping needs to be updated at each frame
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
