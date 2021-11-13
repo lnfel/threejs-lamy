c3d7083900475a3b7f3cd9a883285120da770928
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2021-11-13 14:59:26 +0800

    Materials, Lights, Material Maps and Environment maps

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -1,6 +1,47 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI()
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+// disable magFilter stretching the small gradient
+gradientTexture.minFilter = THREE.NearestFilter
+gradientTexture.magFilter = THREE.NearestFilter
+// we can also deactivate mipmapping
+// try to deactivate mipmaps as long as possible to save gpu
+// we can deeactivate it only if we are using NearestFilter
+gradientTexture.generateMipmaps = false
+
+// p stands for positive
+// n for negative
+// x, y, z for 3d direction
+const environmentMapTexture = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.jpg',
+    '/textures/environmentMaps/0/nx.jpg',
+    '/textures/environmentMaps/0/py.jpg',
+    '/textures/environmentMaps/0/ny.jpg',
+    '/textures/environmentMaps/0/pz.jpg',
+    '/textures/environmentMaps/0/nz.jpg',
+])
 
 /**
  * Base
@@ -10,6 +51,104 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+//const material = new THREE.MeshBasicMaterial()
+//material.map = doorColorTexture
+//material.color.set('yellow')
+//material.color = new THREE.Color('mediumorchid')
+//material.color = new THREE.Color(0x00ff00)
+//material.wireframe = true
+//material.opacity = 0.5
+//material.transparent = true
+//material.alphaMap = doorAlphaTexture
+//material.side = THREE.DoubleSide
+
+//const material = new THREE.MeshNormalMaterial()
+//material.flatShading = true
+
+//const material = new THREE.MeshMatcapMaterial()
+//material.matcap = matcapTexture
+
+//const material = new THREE.MeshDepthMaterial()
+
+//const material = new THREE.MeshLambertMaterial()
+
+//const material = new THREE.MeshPhongMaterial()
+//material.shininess = 100
+// red light reflection
+//material.specular = new THREE.Color(0xff0000)
+
+//const material = new THREE.MeshToonMaterial()
+// cool effect
+// magFilter is strectching the small gradient image by using mipmapping
+//material.gradientMap = gradientTexture
+
+//const material = new THREE.MeshStandardMaterial()
+// when using metalnessMap or RoughnessMap do not use metallness and roughness attributes
+//material.metalness = 0.45
+//material.roughness = 0.65
+//material.map = doorColorTexture
+//material.aoMap = doorAmbientOcclusionTexture
+//material.aoMapIntensity = 10
+// for displacementMap, white color goes up, dark color goes down and gray color does not move
+//material.displacementMap = doorHeightTexture
+//material.displacementScale = 0.1
+//material.metalnessMap = doorMetalnessTexture
+//material.roughnessMap = doorRoughnessTexture
+// add more details to the door
+//material.normalMap = doorNormalTexture
+//material.normalScale.set(0.5, 0.5)
+//material.transparent = true
+//material.alphaMap = doorAlphaTexture
+
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.7
+material.roughness = 0.2
+
+material.envMap = environmentMapTexture
+
+gui.add(material, 'metalness').min(0).max(1).step(0.0001)
+gui.add(material, 'roughness').min(0).max(1).step(0.0001)
+gui.add(material, 'aoMapIntensity').min(0).max(10).step(0.0001)
+gui.add(material, 'displacementScale').min(0).max(1).step(0.0001)
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereBufferGeometry(0.5, 64, 64),
+    material
+)
+sphere.position.x = -1.5
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(1, 1, 100, 100),
+    material
+)
+
+const torus = new THREE.Mesh(
+    new THREE.TorusBufferGeometry(0.3, 0.2, 64, 128),
+    material
+)
+torus.position.x = 1.5
+
+sphere.geometry.setAttribute('uv2', new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2))
+plane.geometry.setAttribute('uv2', new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2))
+torus.geometry.setAttribute('uv2', new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2))
+
+scene.add(sphere, plane, torus)
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
 
 /**
  * Sizes
@@ -65,6 +204,15 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    sphere.rotation.y = 0.1 * elapsedTime
+    plane.rotation.y = 0.1 * elapsedTime
+    torus.rotation.y = 0.1 * elapsedTime
+
+    sphere.rotation.x = 0.15 * elapsedTime
+    plane.rotation.x = 0.15 * elapsedTime
+    torus.rotation.x = 0.15 * elapsedTime
 
     // Update controls
     controls.update()
