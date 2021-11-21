db3ef14964909d97c46e1ecc498f6d521db188d8
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2021-11-21 18:58:52 +0800

    Threejs Lights and LightHelpers, part 14 done

--- 14-lights/src/script.js
+++ 14-lights/src/script.js
@@ -2,6 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js'
 
 /**
  * Base
@@ -18,14 +19,78 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+//const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+const ambientLight = new THREE.AmbientLight()
+ambientLight.color = new THREE.Color(0xffffff)
+ambientLight.intensity = 0.5
 scene.add(ambientLight)
 
-const pointLight = new THREE.PointLight(0xffffff, 0.5)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
+
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3)
+directionalLight.position.set(1, 0.25, 0)
+scene.add(directionalLight)
+
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3)
+scene.add(hemisphereLight)
+
+// THREE.PointLight(color, intensity, distance, decay)
+const pointLight = new THREE.PointLight(0xff9000, 0.5, 3, 2)
+pointLight.position.x = 1
+pointLight.position.y = - 0.5
+pointLight.position.z = 1
 scene.add(pointLight)
+
+// THREE.RectAreaLight(0x4e00ff, 2, width, height)
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 3, 1)
+rectAreaLight.position.set(- 1.5, 0, 1.5)
+rectAreaLight.lookAt(new THREE.Vector3())
+scene.add(rectAreaLight)
+
+// THREE.SpotLight(0x78ff00, 0.5, distance, angle, penumbra, decay)
+// penumbra is like adding soft edge (blur) to spotlight
+const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 7, Math.PI * 0.1, 0.25, 1)
+spotLight.position.set(0, 2, 3)
+scene.add(spotLight)
+
+// to change direction of spotlight we need to modify the target position
+// and also add it to the scene
+//
+// by default spotLight.target is not added to the scene, so if we change
+// target position, nothing happens
+spotLight.target.position.x = - 0.75
+scene.add(spotLight.target)
+
+/**
+ * Light Helpers
+ */
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2)
+scene.add(hemisphereLightHelper)
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
+scene.add(directionalLightHelper)
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
+scene.add(pointLightHelper)
+
+// do not forget to update the spotlightHelper on the next frame when changing spotLight position
+const spotLightHelper = new THREE.SpotLightHelper(spotLight)
+scene.add(spotLightHelper)
+
+window.requestAnimationFrame(() => {
+    spotLightHelper.update()
+})
+
+// RectAreaLightHelper is not included in threejs by default so we need to import it in vendor folder
+// import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js'
+const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight)
+scene.add(rectAreaLightHelper)
+
+window.requestAnimationFrame(() => {
+    rectAreaLightHelper.position.copy(rectAreaLight.position)
+    rectAreaLightHelper.quaternion.copy(rectAreaLight.quaternion)
+    rectAreaLightHelper.update()
+})
 
 /**
  * Objects
