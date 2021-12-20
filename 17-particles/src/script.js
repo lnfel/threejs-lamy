aa493dfaab2af660825d63386cae94ce1ed0de0d
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2021-12-20 22:49:48 +0800

    Generating particles and animating the bad way, part 17 done

--- 17-particles/src/script.js
+++ 17-particles/src/script.js
@@ -19,15 +19,58 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/2.png')
 
 /**
  * Test cube
  */
-const cube = new THREE.Mesh(
+/*const cube = new THREE.Mesh(
     new THREE.BoxBufferGeometry(1, 1, 1),
     new THREE.MeshBasicMaterial()
 )
-scene.add(cube)
+scene.add(cube)*/
+
+/**
+ * Particles
+ */
+// Geometry
+//const particlesGeometry = new THREE.SphereBufferGeometry(1, 32, 32)
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 2500 //500
+
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
+
+for (let i = 0; i < count; i++) {
+    positions[i] = (Math.random() - 0.5) * 10
+    colors[i] = Math.random()
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+
+// Material
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.1, //0.02
+    sizeAttenuation: true,
+    //color: '#ff88cc', // comment this out so it won't mix with vertexColors
+})
+particlesMaterial.transparent = true
+particlesMaterial.alphaMap = particleTexture
+
+// Fixing the particle bug
+//particlesMaterial.alphaTest = 0.001
+//particlesMaterial.depthTest = false
+particlesMaterial.depthWrite = false
+
+// cool effect but use with caution since using blending can have performance issues
+particlesMaterial.blending = THREE.AdditiveBlending
+// Enable vertexColors so that we can apply random colors to each geometry
+particlesMaterial.vertexColors = true
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
 
 /**
  * Sizes
@@ -81,6 +124,18 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update particles
+    //particles.rotation.y = elapsedTime * 0.1
+    // This is a bad idea since we are updating thousands or particles
+    // To do this the right way, utilize custom shaders (see part 24)
+    for (let i = 0; i < count; i++) {
+        const i3 = i * 3
+        const x = particlesGeometry.attributes.position.array[i3 + 0]
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+    }
+
+    particlesGeometry.attributes.position.needsUpdate = true
 
     // Update controls
     controls.update()
