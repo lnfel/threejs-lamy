596234873f2259bb18276ffb604798845a8d88a0
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2022-01-20 23:52:29 +0800

    Part 18 to be continued, resume at 40:10

--- 18-galaxy-generator/src/script.js
+++ 18-galaxy-generator/src/script.js
@@ -18,11 +18,101 @@ const scene = new THREE.Scene()
 /**
  * Test cube
  */
-const cube = new THREE.Mesh(
+/*const cube = new THREE.Mesh(
     new THREE.BoxBufferGeometry(1, 1, 1),
     new THREE.MeshBasicMaterial()
 )
-scene.add(cube)
+scene.add(cube)*/
+
+/**
+ * Galaxy
+ */
+const parameters = {}
+parameters.count = 200
+parameters.size = 0.02
+parameters.radius = 5
+parameters.branches = 3
+
+let geometry = null
+let material = null
+let points = null
+
+const generateGalaxy = () => {
+    /**
+     * Destroy old galaxy
+     */
+    if (points !== null) {
+        geometry.dispose()
+        material.dispose()
+        scene.remove(points)
+    }
+
+    /**
+     * Geometry
+     */
+    geometry = new THREE.BufferGeometry()
+
+    const positions = new Float32Array(parameters.count * 3)
+
+    // Base positions
+    /*for (let i = 0; i < parameters.count; i++) {
+        const i3 = i * 3
+
+        positions[i3 + 0] = (Math.random() - 0.5) * 3
+        positions[i3 + 1] = (Math.random() - 0.5) * 3
+        positions[i3 + 2] = (Math.random() - 0.5) * 3
+    }*/
+
+    // Spiral
+    for (let i = 0; i < parameters.count; i++) {
+        const i3 = i * 3
+
+        const radius = Math.random() * parameters.radius
+        // Example parameters.branches is equal to 3
+        // Modulus 3 means (0,1,2, 0,1,2 and so on)
+        // It won't reach 3 no matter what
+        // Then divide the resulting value to branches again to have a value from 0 to 1
+        // Ex. 0, 0.33, 0.66
+        // Finally multiply the result by PI and 2 to get angle rotation
+        const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
+
+        if (i < 20) {
+            console.log(i, branchAngle)
+        }
+
+        // Sample position of x axis in a single line
+        //positions[i3 + 0] = radius
+
+        positions[i3 + 0] = Math.cos(branchAngle) * radius
+        positions[i3 + 1] = 0
+        positions[i3 + 2] = Math.sin(branchAngle) * radius
+    }
+
+    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+
+    /**
+     * Material
+     */
+    material = new THREE.PointsMaterial({
+        size: parameters.size,
+        sizeAttenuation: true,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending
+    })
+
+    /**
+     * Points
+     */
+    points = new THREE.Points(geometry, material)
+    scene.add(points)
+}
+
+generateGalaxy()
+
+gui.add(parameters, 'count').min(100).max(500).step(50).onFinishChange(generateGalaxy)
+gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
+gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
 
 /**
  * Sizes
