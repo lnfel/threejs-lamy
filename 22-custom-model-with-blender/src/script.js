78b1e76b47fb8fcebcc776f59add57a28fad9e9c
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-03-05 18:02:34 +0800

    Blender basics, create 3D hamburger, part 21 done

--- 22-custom-model-with-blender/src/script.js
+++ 22-custom-model-with-blender/src/script.js
@@ -6,6 +6,93 @@ import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 import * as dat from 'dat.gui'
 
 /**
+ * Blender Shortcuts
+ *   - Orbit Rotate = Rotate Camera
+ *   - Truck and Pedestal = Move camera horizontally and vertically
+ *   - Dolly = Zoom in or out
+ *   - Tilt and Pan = Rotate camera vertically and horizontally
+ *     - Must be in Walk mode to to this (Shift + Back tick)
+ *   - Local View = Focus on selected object and hide others
+ * 
+ * Keyboard:
+ * Reset view = Shift + C
+ * 
+ * Mouse:
+ * Orbit Rotate = Mouse wheel push + Mouse movement
+ * Truck and Pedestal = Shift + Mouse wheel push + Mouse movement
+ * Dolly = Mouse scroll up/forward or down/backward
+ * Shift + Ctrl + Mouse wheel = Go beyond the limits of Dolly/Zoom in and out
+ * 
+ * Trackpad:
+ * Orbit Rotate = 2 Finger + Mouse movement
+ * Truck and Pedestal = Shift + 2 Finger + Mouse movement
+ * Dolly = Pinch using 2 Finger to expand or compress
+ * Dolly = Ctrl + 2 Finger up/forward or down/backward
+ * 
+ * Numpad:
+ * Toggle perspective/ortographic view = numpad 5
+ * Switch Axis view
+ *   - X = numpad 3
+ *   - Y = numpad 1
+ *   - Z = numpad 7
+ * Alternatively pressing Ctrl + Switch Axis numpad buttons
+ * will go to reverse view of corresponding axis
+ * Note: Z axis is at the top in Blender
+ * 
+ * Camera view = numpad 0
+ * Note: The camera is needed when doing renders
+ * 
+ * Focus on selected object = numpad comma
+ * Focus on selected object = View > Frame Selected
+ * 
+ * Local View = numpad / or /
+ * 
+ * Select multiple objects = Shift select object
+ * Note: Active object is bright orange in color
+ * Unselect active object = Shift select object again
+ * Select all = A
+ * Unselect all = Double A
+ * Box like selection = B
+ * Brush like selection = C |Note: Shift click to unselect
+ * 
+ * 3D viewport shortcuts
+ * Object Mode shortcuts
+ * Add Object = Shift + A
+ * Remove object = X
+ * Duplicate object = Shift + D
+ * Bring up object properties = F9 or fn + F9
+ * Hide object = Select object + H
+ * Show hidden objects = Alt + H or Option + H
+ * Hide unselected objects = Shift + H |Note: Similar to Local view without focusing on selected object
+ * Toggle sidebar menu = T or Shift + spacebar to bring popup
+ * Switch Mode Circular menu = Ctrl + Tab
+ * Switch Between object and edit mode = Tab
+ * 
+ * Transforming objects
+ * Position = G
+ * Rotation = R or RR to rotate in all directions
+ * Scale = S
+ * 
+ * Edit Mode shortcuts
+ * Vertex select = 1
+ * Edge select = 2
+ * Face select = 3
+ * Proportional Editing = O
+ * 
+ * Shading
+ *   - Solid = Default with same material for every objects
+ *   - Material = Like Solid shading but with preview for materials
+ *   - Wireframe = Shows only wireframe boundaries
+ *   - Renderer = Low quality render (realistic but less performant)
+ * Shading Mode Circular menu = Z
+ * 
+ * Render = FN + F12
+ * Search = FN + F3
+ * 
+ * Loop Cut = Ctrl + R
+ */
+
+/**
  * Base
  */
 // Debug
---
+++ notes.md
@@ -0,0 +1,28 @@
+# Threejs Journey course 11.2022 update download
+## Thread
+https://freesoff.com/t/get-the-ultimate-three-js-course/10859/21
+https://hacksnation.com/d/12315-download-bruno-simon-threejs-journey-new-updates-11-2022
+## Full Course
+https://drive.google.com/drive/folders/1PbZRyvS-5adH6SWCRkBjIWoOVKznHOVe?usp=sharing
+## Part 7: React Fiber
+https://drive.google.com/file/d/1R7VfUs9n78brSpwuQqkh7uRdgjVVSABP/view?usp=sharing
+Password: AyrtonSenna
+
+# Extract rar files on macos
+https://superuser.com/questions/52124/how-can-i-extract-rar-files-on-the-mac
+```s
+# List commands and version
+unrar
+# Extract the files in its original folder
+unrar x "archive.rar"
+# List files inside the rar file
+unrar l "archive.rar"
+# Extract specific file to desired location
+unrar e archive.rar folder/file.exe desired_location/
+
+unrar e "Three.js Journey - Bruno Simon New Updated 11-2022.part2.rar" "Three.js Journey - Bruno Simon New Updated 11-2022/02. Classic Techniques/21. Scroll based animation.mp4" "Threejs Journey 11.2022/02. Classic Techniques/21. Scroll based animation.mp4"
+
+unrar e "Three.js Journey - Bruno Simon New Updated 11-2022.part1.rar" "Three.js Journey - Bruno Simon New Updated 11-2022" "Threejs Journey 11.2022"
+
+unrar x "Three.js Journey - Bruno Simon New Updated 11-2022.part1.rar"
+```
